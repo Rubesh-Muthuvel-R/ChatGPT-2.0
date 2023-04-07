44c005e70fed456d5e2c74b7af0f67432b469527
Rubesh-Muthuvel-R <2012039@nec.edu.in>
2023-04-08 00:49:07 +0530

    final

--- lib/chatgpt.ts
+++ lib/chatgpt.ts
@@ -1,7 +1,7 @@
 import {Configuration,OpenAIApi} from "openai";
 
 const configuration = new Configuration({
-    apiKey:process.env.OPENAI_API_KEY,
+    apiKey: "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",
 })
 
 const openai = new OpenAIApi(configuration)
